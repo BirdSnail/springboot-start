c9513e3bc6facb7032f0c9ef744e7cac5793f677
yanghuadong <314726028@qq.com>
2019-12-16 17:59:19 +0800

    first commit

---
+++ src/main/java/com/birdsnail/springboot/training/Application.java
@@ -0,0 +1,14 @@
+package com.birdsnail.springboot.training;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author BirdSnail
+ * @date 2019/12/16
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
