248890cdfa3d98dd9bd8ce701b2244a7685cb7b4
yanghuadong <314726028@qq.com>
2019-12-17 10:20:43 +0800

    完成准备工作，包括依赖、插件、配置

--- src/main/java/com/birdsnail/springboot/training/Application.java
+++ src/main/java/com/birdsnail/springboot/training/Application.java
@@ -1,11 +1,13 @@
 package com.birdsnail.springboot.training;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author BirdSnail
  * @date 2019/12/16
  */
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/test/java/com/birdsnail/springboot/training/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.birdsnail.springboot.training;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    void test() {
+        assertEquals("hello world", "hello world");
+    }
+}
